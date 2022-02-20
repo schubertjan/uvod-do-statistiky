d9566af0f6acdb03a1f891dc63ad71b409ed5e9f
schubertj <jan.schubert@kantar.com>
2022-02-21 00:13:07 +0100

    battleship ml

--- scr/battleship.r
+++ scr/battleship.r
@@ -7,29 +7,29 @@ place_ship <- function(dims, ship, board) {
   end_pos_col <- 12
   place_empty <- 1
   counter <<- 0
-  .stop <- 1000
+  .stop <- 100
   # check the place is empty
-  while(place_empty == 1 & counter < .stop) {
-    
+  while (place_empty == 1 & counter < .stop) {
+
     # generate position
-    while((end_pos_row > 8 | end_pos_row < 1) | 
-          (end_pos_col > 8 | end_pos_col < 1) & 
-          counter < .stop) {
+    while ((end_pos_row > 8 | end_pos_row < 1) |
+      (end_pos_col > 8 | end_pos_col < 1) &
+        counter < .stop) {
       # init ship dim
       init_pos_row <- sample(x = 1:dims[1], size = 1)
       init_pos_col <- sample(x = 1:dims[2], size = 1)
       direction <- sample(x = directions, size = 1)
       # calculate ship dims
-      if(direction == "up") {
+      if (direction == "up") {
         end_pos_row <- init_pos_row - (ship - 1)
         end_pos_col <- init_pos_col
-      } else if(direction == "right") {
+      } else if (direction == "right") {
         end_pos_row <- init_pos_row
         end_pos_col <- init_pos_col + (ship - 1)
-      } else if(direction == "down") {
+      } else if (direction == "down") {
         end_pos_row <- init_pos_row + (ship - 1)
         end_pos_col <- init_pos_col
-      } else if(direction == "left") {
+      } else if (direction == "left") {
         end_pos_row <- init_pos_row
         end_pos_col <- init_pos_col - (ship - 1)
       } else {
@@ -38,9 +38,9 @@ place_ship <- function(dims, ship, board) {
       # increase counter
       counter <<- counter + 1
     }
-    
+
     # check if there is already a ship
-    if(sum(board[(init_pos_row:end_pos_row), (init_pos_col:end_pos_col)] > 0)) {
+    if (sum(board[(init_pos_row:end_pos_row), (init_pos_col:end_pos_col)] > 0)) {
       place_empty <- 1
     } else {
       place_empty <- 0
@@ -48,9 +48,9 @@ place_ship <- function(dims, ship, board) {
     # increase counter
     counter <<- counter + 1
   }
-  
+
   # place a ship of while ended
-  if(counter >= .stop) {
+  if (counter >= .stop) {
     stop("Couldn't place a ship on the board")
   } else {
     board[(init_pos_row:end_pos_row), (init_pos_col:end_pos_col)] <- ship
@@ -58,12 +58,12 @@ place_ship <- function(dims, ship, board) {
   }
 }
 
-game <- function(dims = c(8, 8), 
+game <- function(dims = c(8, 8),
                  ships = c(5, 4, 3, 3, 2)) {
   # init game
   board <- matrix(0, nrow = dims[1], ncol = dims[2])
-  
-  for(ship in ships) {
+
+  for (ship in ships) {
     board <- place_ship(dims, ship, board)
   }
   return(board)
--- scr/battleship_ml.r
+++ scr/battleship_ml.r
@@ -2,24 +2,52 @@ source("../scr/battleship.r")
 
 # data shell
 set.seed(42)
-S <- 20000
+S <- 5e5
 dims <- c(8, 8)
 G <- matrix(NA, nrow = S, ncol = dims[1] * dims[2])
 colnames(G) <- as.vector(sapply(LETTERS[1:8], function(i) paste0(i, 1:8)))
+no_converge <- 0
 
+start_t <- Sys.time()
 # play
-for(s in 1:S) {
-  tryCatch({
-    g <- game()
-    g_flat <- as.vector(t(g))
-    G[s, ] <- g_flat 
-  }, error = function(e) {})  
+for (s in 1:S) {
+  tryCatch(
+    {
+      g <- game()
+      g_flat <- as.vector(t(g))
+      G[s, ] <- g_flat
+    },
+    error = function(e) {
+      no_converge <<- no_converge + 1
+    }
+  )
 }
-# throw away games that did not converge
+end_t <- Sys.time()
+end_t - start_t
+no_converge / S
 G <- G[complete.cases(G), ]
+# write.csv(G, "../dats/battleship.csv", row.names = FALSE)
+
+
 # binarize
-G_bin <- data.table::as.data.table(G)
+G_bin <- G
 G_bin[G_bin > 0] <- 1
 
-# fit a model
-m1 <- glm(A1 ~ ., data = G_bin, family = binomial(link = "logit"))
+s <- nrow(G_bin)
+cetnost <- colSums(G_bin)
+rel_cetnost <- cetnost / s
+
+# to tidy
+df <- as.data.frame(rel_cetnost)
+df$cell <- rownames(df)
+df$y <- substr(df$cell, start = 0, stop = 1)
+df$y <- sort(rep(which(LETTERS %in% df$y), dims[1]))
+df$x <- substr(df$cell, start = 2, stop = 3)
+df$y <- as.numeric(df$y)
+df$x <- as.numeric(df$x)
+
+# plot
+library(ggplot2)
+ggplot(df) +
+  geom_tile(aes(x = x, y = y, fill = rel_cetnost)) +
+  scale_fill_distiller(palette = "Spectral")
